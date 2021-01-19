6c41afb39350983750ea921a481790d7f1ff14a6
tsuba-h <tsuba-h@arsaga.jp>
2021-01-19 18:36:11 +0900

    MVVMテンプレート

---
+++ MVVM File.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,12 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ MVVM File.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,28 @@
+//___FILEHEADER___
+
+import Foundation
+
+
+protocol ___FILEBASENAMEASIDENTIFIER___Input {
+    
+}
+
+protocol ___FILEBASENAMEASIDENTIFIER___OutPut {
+    
+}
+
+protocol ___FILEBASENAMEASIDENTIFIER___Type {
+    var inputs: ___FILEBASENAMEASIDENTIFIER___Input {get}
+    var outputs: ___FILEBASENAMEASIDENTIFIER___OutPut {get}
+}
+
+class ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Input, ___FILEBASENAMEASIDENTIFIER___OutPut {
+    
+    init() {
+    }
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Type {
+    var inputs: ___FILEBASENAMEASIDENTIFIER___Input {return self}
+    var outputs: ___FILEBASENAMEASIDENTIFIER___OutPut {return self}
+}
